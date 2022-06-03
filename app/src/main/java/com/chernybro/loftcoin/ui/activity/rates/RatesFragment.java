a8e9e2ad30a5bfd1bdfc4ffa956a7ca70f358eb1
chernybro <anvar-1234567@yandex.ru>
2022-06-03 17:18:05 +0300

    homework1_2 implemention

---
+++ app/src/main/java/com/chernybro/loftcoin/ui/activity/rates/RatesFragment.java
@@ -0,0 +1,23 @@
+package com.chernybro.loftcoin.ui.widget.activity.rates;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.chernybro.loftcoin.R;
+
+
+public class RatesFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_rates, container, false);
+    }
+
+}
